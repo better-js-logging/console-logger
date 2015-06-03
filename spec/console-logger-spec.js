4c52715469ebabd05c8373452552e57dbc898c09
bbottema <b.bottema@projectnibble.org>
2015-06-03 14:48:31 +0000

    added tests vor console logger

--- spec/console-logger-spec.js
+++ spec/console-logger-spec.js
@@ -1,5 +1,66 @@
-/* global describe, beforeEach, expect, it*/
+/* global describe, beforeEach, expect, it, fail*/
 
-describe('angular-logger', function() {
+var gutil = require('gulp-util');
+
+var moment = require('../bower_components/momentjs/moment.js');
+var sprintf = require('../bower_components/sprintf/dist/sprintf.min.js').sprintf;
+var ConsoleLogger = require('../src/console-logger.js').ConsoleLogger;
+
+describe('console-logger', function() {
+
+    var logger = null;
+
+    beforeEach(function resetCounters() {
+        logger = new ConsoleLogger(sprintf, moment);
+    });
     
+    it("should enhance the console with current configuration", function() {
+        logger.datetimePattern = 'YYYY';
+        expectGlobalLog('debug', ['Hello World!'], [moment().year() + '::[global]> ', 'Hello World!']);
+        logger.prefixPattern = ':%2$s:';
+        expectGlobalLog('debug', ['Hello World!'], [':global:', 'Hello World!']);
+        expectGlobalLog('debug', ['Hello World!', 5, { 'this': 'a test'}], [':global:', 'Hello World!', 5, { 'this': 'a test'}]);
+        expectGlobalLog('debug', ['Hello %s! %s', 'World', 'yeah!', 5], [':global:', 'Hello World! yeah!', 5]);
+    });
+    
+    function expectGlobalLog(func, input, output) {
+        var c = fakeConsole(func);
+        logger.enhanceLogging(c);
+        expect(c[func].apply(null, input)).toEqual(output);
+    }
+    
+    it("should provide contextual loggers", function() {
+        var enhancedConsole = fakeConsole('warn');
+        logger.enhanceLogging(enhancedConsole);
+        
+        logger.prefixPattern = ':%2$s:';
+        var localA = enhancedConsole.getLogger('A');
+        
+        logger.prefixPattern = '>%2$s<';
+        var localB = enhancedConsole.getLogger('B');
+        
+        expect(localA.warn('Test [%s]', 'A')).toEqual([':A:', 'Test [A]']);
+        expect(localB.warn('Test [%s]', 'B')).toEqual(['>B<', 'Test [B]']);
+    });
+    
+    function fakeConsole(func) {
+        var c = {
+            debug: function() {fail('unexpected logging function "debug" invoked');},
+            log: function() {fail('unexpected logging function "log" invoked');},
+            info: function() {/*don't fail, is used to display inited info*/},
+            warn: function() {fail('unexpected logging function "warn" invoked');},
+            error: function() {fail('unexpected logging function "error" invoked');}
+        };
+        c[func] = function() {
+            // no fail
+        };
+        return c;
+    }
+    
+    /*
+    console.info('test 1');
+    ConsoleLogger.enhanceLogging(console);
+    console.info('test 2');
+    console.getLogger('local context').info('test 3');
+    */
 });
--- src/console-logger.js
+++ src/console-logger.js
@@ -4,26 +4,34 @@ var LoggingEnhancer = require('../bower_components/better-logging-base/dist/logg
 (function(sprintf, moment) {
     'use strict';
 
-	var ConsoleLogger = function(sprintf, moment) {
+    var ConsoleLogger = function(sprintf, moment) {
         var self = this;
-        
+
         var logEnhancer = new LoggingEnhancer(sprintf, moment);
-        
+
         this.logLevels = logEnhancer.logLevels;
         this.LEVEL = logEnhancer.LEVEL;
-        
+
         this.datetimePattern = 'LLL'; // default datetime stamp pattern, overwrite in config phase
-        this.datetimeLocale = window.navigator.userLanguage || window.navigator.language || 'en';
+        this.datetimeLocale = 'en';
+
+        var hasWindow = typeof window !== 'undefined';
+        this.datetimeLocale = (hasWindow && (window.navigator.userLanguage || window.navigator.language)) || 'en';
+
         this.prefixPattern = '%s::[%s]> '; // default prefix pattern, overwrite in config phase
-	    
+
         // keep originals
-        var logDebug = console.debug;
-        var logLog = console.log;
-        var logInfo = console.info;
-        var logWarn = console.warn;
-        var logError = console.error;
-	        
-	    this.enhanceLogging = function(console) {
+        var logDebug, logLog, logInfo, logWarn, logError;
+
+        this.enhanceLogging = function(console) {
+            if (!logDebug) {
+                logDebug = console.debug;
+                logLog = console.log;
+                logInfo = console.info;
+                logWarn = console.warn;
+                logError = console.error;
+            }
+
             // override global logging functions to add at least a timestamp
             console.trace = logEnhancer.enhanceLogging(bind(console.debug), logEnhancer.LEVEL.TRACE, 'global', self, self.datetimePattern, self.datetimeLocale, self.prefixPattern);
             console.debug = logEnhancer.enhanceLogging(bind(console.debug), logEnhancer.LEVEL.DEBUG, 'global', self, self.datetimePattern, self.datetimeLocale, self.prefixPattern);
@@ -31,7 +39,7 @@ var LoggingEnhancer = require('../bower_components/better-logging-base/dist/logg
             console.info = logEnhancer.enhanceLogging(bind(console.info), logEnhancer.LEVEL.INFO, 'global', self, self.datetimePattern, self.datetimeLocale, self.prefixPattern);
             console.warn = logEnhancer.enhanceLogging(bind(console.warn), logEnhancer.LEVEL.WARN, 'global', self, self.datetimePattern, self.datetimeLocale, self.prefixPattern);
             console.error = logEnhancer.enhanceLogging(bind(console.error), logEnhancer.LEVEL.ERROR, 'global', self, self.datetimePattern, self.datetimeLocale, self.prefixPattern);
-	        
+
             console.getLogger = function(context) {
                 var logger = {
                     trace: logEnhancer.enhanceLogging(bind(logDebug), logEnhancer.LEVEL.TRACE, context, self, self.datetimePattern, self.datetimeLocale, self.prefixPattern),
@@ -43,33 +51,35 @@ var LoggingEnhancer = require('../bower_components/better-logging-base/dist/logg
                 };
                 return logger;
             };
+
+            // check optional dependencies
+            if (!sprintf) {
+                console.warn('[console-logger] sprintf.js not found: https://github.com/alexei/sprintf.js, using fixed layout pattern "%s::[%s]> "');
+            }
+            if (!moment) {
+                console.warn('[console-logger] moment.js not found: http://momentjs.com, using non-localized simple Date format');
+            }
+            
+            var logger = logEnhancer.enhanceLogging(bind(logInfo), logEnhancer.LEVEL.INFO, 'console-logger', this, this.datetimePattern, this.datetimeLocale, this.prefixPattern);
+            logger('logging enhancer initiated');
         };
-        
-        // check optional dependencies
-        if (!sprintf) {
-            console.warn('[console-logger] sprintf.js not found: https://github.com/alexei/sprintf.js, using fixed layout pattern "%s::[%s]> "');
-        }
-        if (!moment) {
-            console.warn('[console-logger] moment.js not found: http://momentjs.com, using non-localized simple Date format');
-        }
-    	
-        var logger = logEnhancer.enhanceLogging(bind(logInfo), logEnhancer.LEVEL.INFO, 'console-logger', this, this.datetimePattern, this.datetimeLocale, this.prefixPattern);
-        logger('logging enhancer initiated');
     };
-        
+
     // manage dependency exports
-	if (typeof module !== 'undefined') {
-		module.exports.ConsoleLogger = ConsoleLogger;
-	} else if (typeof exports !== 'undefined') {
-		exports.ConsoleLogger = ConsoleLogger;
-	} else if (typeof window === 'undefined') {
-		throw new Error('unable to expose ConsoleLogger: no module, exports object and no global window detected');
-	}
-	
-	if (typeof window !== 'undefined') {
-		window.ConsoleLogger = new ConsoleLogger(sprintf, moment);
-	}
-    
+    if (typeof module !== 'undefined') {
+        module.exports.ConsoleLogger = ConsoleLogger;
+    }
+    else if (typeof exports !== 'undefined') {
+        exports.ConsoleLogger = ConsoleLogger;
+    }
+    else if (typeof window === 'undefined') {
+        throw new Error('unable to expose ConsoleLogger: no module, exports object and no global window detected');
+    }
+
+    if (typeof window !== 'undefined') {
+        window.ConsoleLogger = new ConsoleLogger(sprintf, moment);
+    }
+
     function bind(func) {
         return function() {
             func.apply(console, arguments);
